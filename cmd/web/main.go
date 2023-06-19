56ff3ef1947aa21c8c44894985cb22701bedd9f8
Charleen Ipping Wah <charleen.ippingwah@justeattakeaway.com>
2023-06-19 20:31:27 +0100

    Adding test for Render package II

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -27,7 +27,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(fmt.Sprintf("Starting application on %s", portNumber))
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
 	srv := &http.Server{
 		Addr:    portNumber,
@@ -40,34 +40,34 @@ func main() {
 	}
 }
 
-
 func run() error {
-		// what am I going to put in the session
-		gob.Register(models.Reservation{})
-		// change this to true when in production
-		app.InProduction = false
-	
-		// set up the session
-		session = scs.New()
-		session.Lifetime = 24 * time.Hour
-		session.Cookie.Persist = true
-		session.Cookie.SameSite = http.SameSiteLaxMode
-		session.Cookie.Secure = app.InProduction
-	
-		app.Session = session
-	
-		tc, err := render.CreateTemplateCache()
-		if err != nil {
-			log.Fatal("cannot create template cache")
-			return err
-		}
-	
-		app.TemplateCache = tc
-		app.UseCache = false
-	
-		repo := handlers.NewRepo(&app)
-		handlers.NewHandlers(repo)
-	
-		render.NewTemplates(&app)
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	// change this to true when in production
+	app.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+		return err
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
 	return nil
 }
