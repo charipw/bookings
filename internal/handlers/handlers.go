8238d74c333bcf22027706a612e4940a1c80d450
Charleen Ipping Wah <charleen.ippingwah@justeattakeaway.com>
2023-06-21 13:19:21 +0100

    Connecting search availability to the make reservation page

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -14,6 +14,7 @@ import (
 	"github.com/charipw/bookings/internal/render"
 	"github.com/charipw/bookings/internal/repository"
 	"github.com/charipw/bookings/internal/repository/dbrepo"
+	"github.com/go-chi/chi"
 )
 
 // Repo the repository used by the handlers
@@ -241,3 +242,28 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 		Data: data,
 	})
 }
+
+func (m *Repository) ChooseRoom(w http.ResponseWriter, r *http.Request) {
+	roomID, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+	
+	res, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	res.RoomID = roomID
+
+	m.App.Session.Put(r.Context(), "reservation", res)
+
+	http.Redirect(w, r,"/make-reservation", http.StatusSeeOther)
+}
+
+
+
+
+
