96c82615dfcc3ecda027f4f40f332ea2f6dd33ed
Charleen Ipping Wah <charleen.ippingwah@justeattakeaway.com>
2023-06-23 11:49:54 +0100

    Converting the admin template into a Go template

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -36,7 +36,7 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/user/login", handlers.Repo.ShowLogin)
 	mux.Post("/user/login", handlers.Repo.PostShowLogin)
-	mux.Post("/user/logout", handlers.Repo.Logout)
+	mux.Get("/user/logout", handlers.Repo.Logout)
 
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(Auth)
